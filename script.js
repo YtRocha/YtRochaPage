693b9a3c6d9c83534c63e467bad3fbb74fca6539
YtRocha <ytallo4gamer@gmail.com>
2022-02-22 15:47:34 -0300

    reframe function to hide/show div information

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let button_arrow = document.getElementById("button_arrow");
 
 button_arrow.onclick = function () {
     let div = document.getElementById("information");
+    let text = document.getElementById("text_unlocknw");
     if (div.style.display !== "block") {
         div.style.display = "block";
+        text.style.display = "none";
+        button_arrow.style.display = "none";
     } else {
         div.style.display = "none";
     }
